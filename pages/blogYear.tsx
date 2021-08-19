706f203ccebf3e0b620ba71b3e04dfc78029a284
Matt Seidholz <mseidholz@gmail.com>
2021-08-19 14:43:04 -0400

    Remove non-functioning scroll-up detector...

--- pages/blogYear.tsx
+++ pages/blogYear.tsx
@@ -1,74 +1,57 @@
 import React from "react";
-import { useEffect } from "react";
+
 import styled, { keyframes } from "styled-components";
 import { QUERIES } from "../styles/CONSTANTS";
 
 function BlogYear({ year }) {
   const yearRef = React.useRef(null);
-  const [scrollValue, setScrollValue] = React.useState<number>(0);
-
-  const [status, setStatus] = React.useState<string | undefined>();
+  const [status, setStatus] = React.useState<string | undefined>("idle");
   function handleScroll() {
     const rect = yearRef.current.getBoundingClientRect();
-    const { y, height } = rect;
+    const { y } = rect;
 
-    const scrollingValue = document.documentElement.scrollTop;
-
-    if (scrollingValue > scrollValue) {
-      if (y < 200 && y > 0) {
-        setStatus("top");
-      }
-      if (y < 96) {
-        setStatus("fading");
-      }
+    if (y < 180 && y > 0) {
+      setStatus("top");
     }
-    setScrollValue(() => (scrollingValue <= 0 ? 0 : scrollingValue));
+
+    // ie - we are scrolling down
+    if (y < 96 && status !== "gone") {
+      setStatus("fading");
+    }
+
+    // ie - we are scrolling up
+    if (y < 96 && status === "gone") {
+      setStatus("top");
+    }
+
+    if (y < 60) {
+      setStatus("gone");
+    }
+
+    if (y > 170) {
+      setStatus("idle");
+    }
   }
 
   React.useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    setStatus("idle");
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <Year
-      status={status}
-      ref={yearRef}
-      style={{ "--yearTextOpacity": status === "top" ? 1 : 0 }}
-    >
-      <FadeOutDiv
-        style={{
-          "--underlineOpacity": status === "top" || status === "idle" ? 1 : 0,
-        }}
-        status={status}
-      >
-        {year}
-      </FadeOutDiv>
+    <Year status={status} data={status} ref={yearRef}>
+      <FadeOutDiv status={status}>{year}</FadeOutDiv>
+      <YearBar status={status} barData={status} />
     </Year>
   );
 }
 
-const fadeIn = keyframes`
-0% {opacity: 0;}
-100% {opacity: 1;}
-`;
-
-const fadeOut = keyframes`
-0% {opacity: 1;
-}
-100% {opacity: 0;
-transform: translateY(-12px);
-transform: translateX(-4px);}
-`;
-
 const FadeOutDiv = styled.div`
-  animation: ${(props) => {
-      return props.status === "fading" ? fadeOut : null;
-    }}
-    0.3s linear;
-  animation-iteration-count: 1;
-  opacity: var(--underlineOpacity);
+  ${(props) =>
+    props.status === "fading" ? `transform: translateY(-6px);` : null};
+  opacity: ${(props) =>
+    props.status === "fading" || props.status === "gone" ? 0 : 1};
+  transition: opacity 0.6s, transform 0.3s;
 `;
 
 const Year = styled.div`
@@ -80,23 +63,25 @@ const Year = styled.div`
   background-color: #fff;
   font-weight: 700;
   margin-bottom: 24px;
-
   @media ${QUERIES.phone} {
     margin-left: -1.5rem;
   }
+`;
 
-  &:after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 1px;
-    bottom: 1px;
-    left: -45px;
-    z-index: -1;
-    transform: scale(0.9);
-    background-color: #dcdcdc;
-    opacity: var(--yearTextOpacity);
-  }
+const YearBar = styled.div`
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  bottom: 1px;
+  left: -45px;
+  z-index: -1;
+  transform: scale(0.9);
+  background-color: #dcdcdc;
+  opacity: ${(props) => {
+    return props.barData === "top" ? 0.6 : 0;
+  }};
+  transition: opacity 0.3s;
 `;
 
 export default React.memo(BlogYear);
